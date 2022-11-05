e54681aef6c1680145e3f4db0a9650e85119fad8
Slava <rybco@outlook.com>
2022-11-06 02:57:51 +0300

    highlights and jdtls fix

--- init.lua
+++ init.lua
@@ -2,13 +2,7 @@ require('base')
 require('plugins')
 require('maps')
 require('commands')
-
-vim.cmd([[
-  syntax on
-  colors gruvbox
-  hi Normal guibg=NONE ctermbg=NONE
-  hi Error NONE
-]])
+require('highlights')
 
 vim.g.coq_settings = {
   auto_start = 'shut-up',
---
+++ lua/highlights.lua
@@ -0,0 +1,6 @@
+vim.cmd([[
+  syntax on
+  colors gruvbox
+  hi Normal guibg=NONE ctermbg=NONE
+  hi Error NONE
+]])
